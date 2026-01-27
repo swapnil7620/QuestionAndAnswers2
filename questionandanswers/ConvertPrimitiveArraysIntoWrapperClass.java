f0076b426712bf57bf5cdd62fe79495d7045d684
swapnil <130269745+swapnil7620@users.noreply.github.com>
2026-01-27 12:36:45 +0530

    Convert Primitive Arrays Into Wrapper Class

--- questionandanswers/ConvertPrimitiveArraysIntoWrapperClass.java
+++ questionandanswers/ConvertPrimitiveArraysIntoWrapperClass.java
@@ -1,17 +1,27 @@
 package com.java.questionandanswers;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class ConvertPrimitiveArraysIntoWrapperClass {
     public static void main(String[] args) {
-        int[] primitiveValue ={1,2,4,7,3,20,5};
 
+        // first way to convert primitive array to Wrapper class
+
+        int[] primitiveValue ={1,2,4,7,3,20,5};
         Integer[] wrapperValue = new Integer[primitiveValue.length];
-        //    System.out.println(Arrays.toString(wrapperValue));
 
         for(int i =0; i<primitiveValue.length; i++){
             wrapperValue[i] = primitiveValue[i];
         }
         System.out.print(Arrays.toString(wrapperValue));
+
+        // second way  to   convert primitive array to Wrapper class
+        // using stream
+
+        int[] number ={10,212,200,340,5,40};
+
+        Integer[] value = Arrays.stream(number).boxed().toArray(Integer[]::new);
+        System.out.print(Arrays.toString(value));
     }
 }
